7cbe2385e028d129c6cb876cc710683de709370d
Alex Walker <awalker2@stevens.edu>
2020-05-05 18:15:46 -0400

    import fix

--- pi-proxy-project/Addons/models/JSONLogger.py
+++ pi-proxy-project/Addons/models/JSONLogger.py
@@ -1,10 +1,9 @@
 from mitmproxy import ctx, http
 
 class JSONLogger:
-        def __init__(self):
-            self.log = ctx.log
-
         def request(self, flow: http.HTTPFlow) -> None:
                 host = flow.request.pretty_host
                 httpVer = flow.request.http_version
                 
+                print(host)
+                print(httpVer)
