aff3fbf04fc38628d78af88c3d6013ed04a8f8a6
Alex Walker <awalker2@stevens.edu>
2020-05-05 02:45:24 -0400

    Started project

--- pi-proxy-project/Addons/Load_Basic.py
+++ pi-proxy-project/Addons/Load_Basic.py
@@ -1,14 +1,15 @@
+import mitmproxy.http
 from mitmproxy import ctx
 
 class BasicAddon:
         def __init__(self):
                 self.num = 0
 
-        def request(self, flow):
+        def request(self, flow: mitmproxy.http.HTTPFlow):
                 self.num = self.num + 1
                 ctx.log.info("We've seen %d flows" % self.num)
 
-        def response(self, flow):
+        def response(self, flow: mitmproxy.http.HTTPFlow):
                 if flow.response.headers.get('Content-Type','').startswith("text/html"):
                         flow.response.text = flow.response.text.replace("test", "new word")
 
