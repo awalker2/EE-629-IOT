70e5a264b73f536413687e56c45bf423642d7305
Alex Walker <awalker2@stevens.edu>
2020-05-05 21:42:25 -0400

    adblocker

--- pi-proxy-project/Addons/models/AdBlocker.py
+++ pi-proxy-project/Addons/models/AdBlocker.py
@@ -5,14 +5,29 @@ class AdBlocker:
         def __init__(self):
                self.blockedHostnames = [
                 "pagead2.googlesyndication.com",
-               "securepubads.g.doubleclick.net"
+                "securepubads.g.doubleclick.net",
+                "www.googleadservices.com"
+               ]
+
+               self.blockedPaths = [
+                "/pagead/"
                ]
 
 
         def request(self, flow: mitmproxy.http.HTTPFlow):
+                blocked = False
                 host = flow.request.host
-                ctx.log.info("Checking: " + host)
+                path = flow.request.path
+
+                # Check blocked hostnames
                 if host in self.blockedHostnames:
-                        ctx.log.info("Blocked request to: " + host)
+                        blocked = True
+                # Check blocked path substrings
+                for blockedPath in self.blockedPaths:
+                        if blockedPath in path:
+                                blocked = True
+
+                if (blocked):
                         flow.kill()
+                        ctx.log.info("Blocked request to: " + host)
 
